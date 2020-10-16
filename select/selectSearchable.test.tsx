436fd88a61c3490210e486dc100e0b419d25a242
Artur <arturbani@gmail.com>
2020-10-16 14:31:50 -0300

    feature(ci): configure travis ci e basic jest setup

---
+++ select/selectSearchable.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react";
+import { items } from "../test/mocks/items";
+import { SelectSearchable } from ".";
+
+describe("Component: SelectSearchable", () => {
+  describe("rendering", () => {
+    it("should render and search in SelectSearchable", async () => {
+      const { getByRole } = render(
+        <SelectSearchable
+          items={items}
+          onSelectedItem={() => {}}
+          hasSearch={true}
+          noOptionsMessage="Nenhuma opção encontrada"
+        />
+      );
+      const childrens = getByRole("list").children;
+      const input = getByRole("textbox") as HTMLInputElement;
+
+      expect(childrens.length).toEqual(items.length);
+
+      act(() => {
+        fireEvent.change(input, { target: { value: "Teste" } });
+      });
+
+      expect(childrens[0].textContent).toEqual("Nenhuma opção encontrada");
+
+      act(() => {
+        fireEvent.change(input, { target: { value: "Forró" } });
+      });
+
+      expect(childrens[0].textContent).toEqual("Forró");
+    });
+  });
+});
