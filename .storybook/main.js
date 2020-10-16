1c48abb4c1b45fa234c49a86180311fcc1a48210
carolinasamp <carolina.sampaio@ymail.com>
2020-10-16 12:46:42 -0300

    adicionando storybook como dependencia de dev

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "stories": [
+    "../select/*.stories.@(mdx|js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
---
+++ select/selectSearchable.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import SelectSearchable from "./selectSearchable";
+import { items } from "../app";
+
+export default {
+  title: "SelectSearchable",
+  component: SelectSearchable
+};
+
+const Template = args => <SelectSearchable {...args} />;
+
+export const Searchable = Template.bind({});
+Searchable.args = {
+  items,
+  onSelectedItem: () => {}
+};
+
+export const SearchableWithSearch = Template.bind({});
+SearchableWithSearch.args = {
+  items,
+  onSelectedItem: () => {},
+  hasSearch: true
+};
+
+export const SearchableWithPlaceholder = Template.bind({});
+SearchableWithPlaceholder.args = {
+  items,
+  onSelectedItem: () => {},
+  hasSearch: true,
+  placeholder: "Valor esperado"
+};
